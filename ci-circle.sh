b0a19ac6392710b3f33d72158dff00acc92bd0f9
hongsumfyp <46188781+hongsumfyp@users.noreply.github.com>
2018-12-29 00:06:30 +0800

    Update ci-circle.sh

--- ci-circle.sh
+++ ci-circle.sh
@@ -37,6 +37,9 @@ else
 fi
 
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+echo 'export PATH="/home/circleci/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
 pyenv install 3.5.2
 
