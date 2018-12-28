944fc848ea77a3d09513bbe5c4540e93038f462c
hongsumfyp <46188781+hongsumfyp@users.noreply.github.com>
2018-12-29 00:03:44 +0800

    Update ci-circle.sh

--- ci-circle.sh
+++ ci-circle.sh
@@ -36,11 +36,7 @@ else
     echo Python is not installed
 fi
 
-git clone git://github.com/yyuu/pyenv.git /home/circleci/.pyenv
-echo 'export PATH="/home/circleci/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bashrc
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
 pyenv install 3.5.2
 
